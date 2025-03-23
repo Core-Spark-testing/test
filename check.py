86127caf7995ddc8e56c4060ee10b775a770e6fc
Karthik2697 <karthikeyang@coresolutionsinc.com>
2025-03-23 13:43:40 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,18 @@
-print("This is for testing")
+#!/usr/bin/env python3
+from app import create_app, create_socketio
+from config import Config
+
+# Create Flask app
+app = create_app()
+
+# Create SocketIO instance
+socketio = create_socketio(app)
+
+if __name__ == '__main__':
+    config = Config()
+    debug = config.get('flask.debug', False)
+    host = '0.0.0.0'
+    port = 5000
+    
+    # Start the server
+    socketio.run(app, host=host, port=port)
